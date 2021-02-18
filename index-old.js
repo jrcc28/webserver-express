082a92eac19bf3fe7875572a4630488a6c74c56c
Rodolfo <jrcc28@hotmail.com>
2021-02-18 14:52:55 -0600

    Modifications and new page

--- index-old.js
+++ index-old.js
@@ -1,6 +1,14 @@
 const http = require('http');
 
 http.createServer((req, resp) => {
+    // resp.serHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    // resp.writeHead(200, { 'Content-Type': 'application/csv' });
+    // resp.write('id, name\n');
+    // resp.write('1, Jose\n');
+    // resp.write('2, Jose\n');
+    // ...
+    // resp.end();
+
     resp.writeHead(200, { 'Content-Type': 'application/json' });
     let salida = {
         nombre: 'Jose',
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 /**
  * Middleware: Instruccion/callback que se va a ejecutar siempre sin importar el url solicitado
  */
-app.use(express.static(__dirname + '/public')) // Folder to use public or static. Everyone can see this folder and it's content
+app.use(express.static(__dirname + '/public')) // Folder to use public or static. Everyone can see this folder and its content
 
 // Handlebars: Express hbs engine
 // __dirname is a global name of the path
@@ -21,12 +21,29 @@ app.set('view engine', 'hbs');
 // Be carefull when use static server or server service "Get /". Could create errors
 app.get('/', (req, res) => {
     res.render('home', {
-        name: 'Rodolfo Calvo'
+        name: 'Rodolfo Calvo',
+        tittle: 'Learning NodeJS'
     }); // home.hbs
+})
+
+app.get('/generic', (req, res) => {
+    // res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic');
+})
+
+app.get('/elements', (req, res) => {
+    res.render('elements');
+    // res.sendFile(__dirname + '/public/elements.html');
 })
 
 app.get('/about', (req, res) => {
     res.render('about'); // about.hbs
+})
+
+app.get('*', (req, res) => {
+    // res.send('404 | Page not found');
+    // res.sendFile(__dirname + '/public/404.hbs');
+    res.render('404'); // 404.hbs
 })
 
 // app.get('/data', (req, res) => {
